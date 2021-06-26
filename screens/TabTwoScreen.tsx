b4ccaf5c2fe1c2060f90ce88bc7fe3493dc59b48
James Sinkler <jamessinkler713@gmail.com>
2021-06-26 16:04:45 -0700

    4 exercises now chosen randomly, timer in place to start on button press

--- screens/TabTwoScreen.tsx
+++ screens/TabTwoScreen.tsx
@@ -4,35 +4,66 @@ import { StyleSheet, Button } from 'react-native';
 import EditScreenInfo from '../components/EditScreenInfo';
 import { Text, View } from '../components/Themed';
 import Timer from '../components/Timer';
+// import chooseExercise from '../utils/chooseExercise'
+import Exercises from '../constants/Exercises'
+
+console.log(Exercises)
 
 type Exercise = {
   name: string;
   reps: number;
 }
 
-const arrayOfExercises: Exercise[] = [{name: 'pushups', reps: 10}, {name: 'squats', reps: 15}, {name: 'rope climbers', reps: 30}]
+//const arrayOfExercises: Exercise[] = [{name: 'pushups', reps: 10}, {name: 'squats', reps: 15}, {name: 'rope climbers', reps: 30}]
 
-function chooseExercise(): Exercise {
-  let randomNum: number = Math.floor(Math.random() * 3)
-  let exercise = arrayOfExercises[randomNum]
-  return exercise
+function chooseExercises(exercises: Exercise[]): Exercise[] {
+  let chosenExercises: Exercise[] = []
+  for (let i=0; i<4; i++) {
+    let randomNum = Math.floor(Math.random() * exercises.length)
+    let exercise:Exercise = exercises[randomNum]
+    // remove from exercise so not chosen twice
+    exercises = exercises.filter(item => item.name !== exercise.name)
+    chosenExercises.push(exercise)
+    console.log(chosenExercises)
+  }
+  return chosenExercises
 }
 
 export default function TabTwoScreen() {
-  const [exercise, setExercise] = React.useState({} as Exercise)
+  const [exercise, setExercise] = React.useState([] as Exercise[])
+  const [startTimer, setStartTimer] = React.useState(false)
+  const [newExercises, setNewExercises] = React.useState(false)
+
   React.useEffect(()=> {
-    let chosenExercise: Exercise = chooseExercise()
-    setExercise(chosenExercise)
+    let chosenExercises: Exercise[] = chooseExercises(Exercises)
+    setExercise(chosenExercises)
   }, [])
+
+  React.useEffect(()=> {
+    if (newExercises === true) {
+      let chosenExercises: Exercise[] = chooseExercises(Exercises)
+      setExercise(chosenExercises)
+      setNewExercises(false)
+    }
+  }, [newExercises])
+
+  let exercises = exercise.map((item) => (
+    <>
+      <Text style={styles.exerciseTitle}>{item.name}</Text>
+      <Text style={styles.reps}>reps {item.reps}</Text>
+      <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
+    </>
+  ))
+
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>Hey this is your exercise</Text>
+      <Text style={styles.title}>Today's exercises</Text>
+      <Button style={styles.exerciseButton} title='Get different exercises' onPress={()=> { setNewExercises(true)}} />
       <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
-      <Text style={styles.title}>{exercise.name}</Text>
-      <Text style={styles.title}>reps {exercise.reps}</Text>
-      <Button onPress={()=> {}} title='Start Workout' />
+      {exercises}
+      <Button onPress={()=> { setStartTimer(true)}} title='Start Workout' />
       <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
-      <Timer done={false} startTime={true} />
+      <Timer done={false} startTime={startTimer} />
     </View>
   );
 }
@@ -46,10 +77,24 @@ const styles = StyleSheet.create({
   title: {
     fontSize: 20,
     fontWeight: 'bold',
+    marginBottom: '10px',
+  },
+  exerciseTitle: {
+    fontSize: 24,
+    marginBottom: '8px',
+  },
+  reps: {
+    fontSize: 16,
   },
   separator: {
-    marginVertical: 30,
+    marginVertical: 8,
     height: 1,
     width: '80%',
   },
+  text: {
+    marginBottom: '10px',
+  },
+  exerciseButton: {
+    marginVertical: 8,
+  }
 });

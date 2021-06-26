12cd55c7fb3a713c9168e7851ee9e669f505580a
James Sinkler <jamessinkler713@gmail.com>
2021-06-26 16:03:02 -0700

    some hardcoded exercises to choose for app to chose from randomly

---
+++ constants/Exercises.ts
@@ -0,0 +1,24 @@
+type Exercise = {
+  name: string;
+  reps: number;
+}
+
+const Exercises: Exercise[] = 
+  [
+    {name: 'pushups', reps: 10},
+    {name: 'squats', reps: 15},
+    {name: 'rope climbers', reps: 30},
+    {name: 'burpees', reps: 10},
+    {name: 'high knees', reps: 20},
+    {name: 'pushup position to plank position', reps: 10},
+    {name: 'kettlebell swings', reps: 15},
+    {name: 'standing rows w/ rings', reps: 10},
+    {name: 'jumping jacks', reps: 20},
+    {name: 'overhead press', reps: 10},
+    {name: 'mountain climbers', reps: 20},
+    {name: 'deadlifts kettlebell', reps: 10},
+    {name: 'planche pushups', reps: 5},
+    {name: 'wide pushups', reps: 10},
+]
+
+export default Exercises

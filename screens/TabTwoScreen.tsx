2169ff4179539841b6dada558782b1493f51c767
James Sinkler <jamessinkler713@gmail.com>
2021-06-25 22:22:35 -0700

    loads up a random exercise when component mounts

--- screens/TabTwoScreen.tsx
+++ screens/TabTwoScreen.tsx
@@ -4,12 +4,31 @@ import { StyleSheet } from 'react-native';
 import EditScreenInfo from '../components/EditScreenInfo';
 import { Text, View } from '../components/Themed';
 
+type Exercise = {
+  name: string;
+  reps: number;
+}
+
+const arrayOfExercises: Exercise[] = [{name: 'pushups', reps: 10}, {name: 'squats', reps: 15}, {name: 'rope climbers', reps: 30}]
+
+function chooseExercise(): Exercise {
+  let randomNum: number = Math.floor(Math.random() * 3)
+  let exercise = arrayOfExercises[randomNum]
+  return exercise
+}
+
 export default function TabTwoScreen() {
+  const [exercise, setExercise] = React.useState({} as Exercise)
+  React.useEffect(()=> {
+    let chosenExercise: Exercise = chooseExercise()
+    setExercise(chosenExercise)
+  }, [])
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>Tab Two</Text>
+      <Text style={styles.title}>Hey this is your exercise</Text>
       <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
-      <EditScreenInfo path="/screens/TabTwoScreen.tsx" />
+      <Text style={styles.title}>{exercise.name}</Text>
+      <Text style={styles.title}>reps {exercise.reps}</Text>
     </View>
   );
 }

b2a7c9e3cfb17ae62d811ba6df4821dfa1f09fbe
James Sinkler <jamessinkler713@gmail.com>
2021-06-26 17:33:30 -0700

    play with flex and views/SafeAreaViews

--- screens/TabOneScreen.tsx
+++ screens/TabOneScreen.tsx
@@ -1,5 +1,6 @@
 import * as React from 'react';
 import { StyleSheet, Button } from 'react-native';
+import { SafeAreaView } from 'react-native-safe-area-context';
 
 import EditScreenInfo from '../components/EditScreenInfo';
 import { Text, View } from '../components/Themed';
@@ -8,11 +9,10 @@ import TabTwoScreen from './TabTwoScreen'
 
 export default function TabOneScreen({navigation}) {
   return (
-    <View style={styles.container}>
+      <View style={styles.container}>
       <Text style={styles.title}>Time to Workout</Text>
       <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
       <Button title='Get Workout' onPress={()=> {navigation.navigate('TabTwo')}}>Get Exercises</Button>
-
     </View>
   );
 }
@@ -20,15 +20,20 @@ export default function TabOneScreen({navigation}) {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    alignItems: 'center',
     justifyContent: 'center',
+    alignItems: 'center',
+  },
+  innerContainer: {
+    flex: -1,
+    alignItems: 'center',
+    justifyContent: 'space-between',
   },
   title: {
     fontSize: 20,
     fontWeight: 'bold',
   },
   separator: {
-    marginVertical: 30,
+    marginVertical: 15,
     height: 1,
     width: '80%',
   },

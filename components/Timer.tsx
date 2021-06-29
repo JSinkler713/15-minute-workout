6cbbc014cec86332bcf215d8a84ac3ac49d99bc2
James Sinkler <jamessinkler713@gmail.com>
2021-06-28 17:40:42 -0700

    adds Audio when timer is at 0

--- components/Timer.tsx
+++ components/Timer.tsx
@@ -1,52 +1,69 @@
-import React, {useState, useRef, useEffect} from 'react'
-import {StyleSheet, View, Text} from 'react-native'
+import React, { useState, useRef, useEffect } from "react";
+import { StyleSheet, View, Text } from "react-native";
+import { Audio } from "expo-av";
 
-const Timer = ({done, updateTime, startTime, pause, restart})=> {
-  const [time, setTime] = useState(15*60)
+const Timer = ({ done, updateTime, startTime, pause, restart }) => {
+  const [sound, setSound] = useState<any>();
+  const [time, setTime] = useState(15 * 60);
   // timeRef.current starts at 15 minutes
-  const [intervalId, setIntervalId] = useState()
-  let timeRef = useRef(15*60)
+  const [intervalId, setIntervalId] = useState();
+  let timeRef = useRef(15 * 60);
 
+  async function playSound() {
+    console.log("Loading Sound");
+    const { sound } = await Audio.Sound.createAsync(
+                      require("../assets/sounds/Done.mp3")
+    );
+    setSound(sound);
+    console.log("Playing Sound");
+    await sound.playAsync();
+  }
 
   // should run when startTime changes
-  useEffect(()=> {
+  useEffect(() => {
     if (!done && startTime) {
-      const timerId = setInterval(()=> {
-        console.log('updating time')
-        timeRef.current--
+      const timerId = setInterval(() => {
+        console.log("updating time");
+        timeRef.current--;
         // pass it back up to App
         //updateTime(timeRef.current)
-        setTime(timeRef.current)
-      }, 1000)
-      setIntervalId(timerId)
-      return ()=> {clearInterval(timerId)}
+        setTime(timeRef.current);
+      }, 1000);
+      setIntervalId(timerId);
+      return () => {
+        clearInterval(timerId);
+      };
     }
-  }, [startTime])
+  }, [startTime]);
 
-  useEffect(()=> {
+  useEffect(() => {
     if (time === 0) {
-      console.log('Its time')
-      clearInterval(intervalId)
+      console.log("Its time");
+      clearInterval(intervalId);
+      //playSound
+      playSound();
     } else {
-      console.log(time)
-      console.log(timeRef)
+      console.log(time);
+      console.log(timeRef);
     }
-  }, [setTime, time])
+  }, [setTime, time]);
 
-  return(
+  return (
     <View>
-      <Text style={styles.timerStyle}>{Math.floor(time / 60)}:{Math.floor((time % 60)/10)}{time % 10}</Text>
+      <Text style={styles.timerStyle}>
+        {Math.floor(time / 60)}:{Math.floor((time % 60) / 10)}
+        {time % 10}
+      </Text>
     </View>
-  )
-}
+  );
+};
 
 const styles = StyleSheet.create({
- timerStyle: {
+  timerStyle: {
     fontSize: 32,
-    fontWeight: 'bold',
+    fontWeight: "bold",
     marginBottom: 10,
-  }
+  },
 });
 
-export default Timer
-
+export default Timer;

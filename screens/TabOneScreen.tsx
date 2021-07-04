093afdf00d6e43c198cc8eb7786277d9b5402eb0
James Sinkler <jamessinkler713@gmail.com>
2021-07-04 11:22:44 -0600

    add bottom banner add for android mobile, TODO: fix for web & ios

--- screens/TabOneScreen.tsx
+++ screens/TabOneScreen.tsx
@@ -5,14 +5,41 @@ import { SafeAreaView } from 'react-native-safe-area-context';
 import EditScreenInfo from '../components/EditScreenInfo';
 import { Text, View } from '../components/Themed';
 import TabTwoScreen from './TabTwoScreen'
+import {
+  AdMobBanner,
+  AdMobInterstitial,
+  PublisherBanner,
+  AdMobRewarded,
+  setTestDeviceIDAsync,
+} from "expo-ads-admob";
 
 
 export default function TabOneScreen({navigation}) {
+
+  React.useEffect(()=> {
+    const setDevice = async()=> {
+      await setTestDeviceIDAsync('EMULATOR');
+    }
+    setDevice()
+  }, [])
+
+  function bannerError() {
+    console.log('oops')
+    return
+  }
   return (
       <View style={styles.container}>
       <Text style={styles.title}>Time to Workout</Text>
       <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
       <Button title='Get Workout' onPress={()=> {navigation.navigate('Workout')}}>Get Exercises</Button>
+      <AdMobBanner
+          style={styles.bottomBanner}
+          bannerSize="banner"
+          adUnitID="ca-app-pub-3940256099942544/6300978111"
+          // Test ID, Replace with your-admob-unit-id
+          // testDeviceID="EMULATOR"
+          onDidFailToReceiveAdWithError={bannerError}
+        />
     </View>
   );
 }
@@ -36,5 +63,9 @@ const styles = StyleSheet.create({
     marginVertical: 15,
     height: 1,
     width: '80%',
+  },
+  bottomBanner: {
+    position: "absolute",
+    bottom: 0
   },
 });
